f8ac6c015706828442f9051436498309e472709e
Nick Howlett <nchowlett@pm.me>
2023-07-18 11:17:19 +1000

    Updated: LES dynK model with better boundary

--- foamCases/LES-dynK/README.md
+++ foamCases/LES-dynK/README.md
@@ -1,4 +1,4 @@
 # Model: LES dynKEqn
 Model is simplified for efficient testing:  
- 	- 2-dimensional representation (use our geom/mesh file: mesh2dJet_noShield_noHeads.py).  
+ 	- 2-dimensional representation (use our geom/mesh file: mesh2dJet_withShield_noHeads.py).  
  	- No shield object modelled in domain (OpenFoam's baffles not created).  
--- geometryMesh/mesh2dJet_withShield_noHeads.py
+++ geometryMesh/mesh2dJet_withShield_noHeads.py
@@ -22,12 +22,11 @@ gmsh.clear()
 # MODIFY: major parameters [mm]. You will also need to adjust further mesh 
 # parameters below (specifically grid grading of 'all other surfaces' & 
 # 'streamwise direction').
-nameModel = "mesh2dJet_withShield_noHeads"
+nameModel = "mesh2dJet_withShield_noHeads_case3"
 mouthHeight = 20
 shieldLength =200
 distanceMouthToShield = 1500
 distanceBehindShield = 500
-meshSize = 70
 depth = 70
 
 
@@ -37,10 +36,10 @@ geoMod = gmsh.model.geo
 
 
 # mouth surface & grid
-P1 = geoMod.addPoint(190, 0, 0, meshSize)
-P2 = geoMod.addPoint(190, depth, 0, meshSize)
-P3 = geoMod.addPoint(210, 0, 0, meshSize)
-P4 = geoMod.addPoint(210, depth, 0, meshSize)
+P1 = geoMod.addPoint(190, 0, 0)
+P2 = geoMod.addPoint(190, depth, 0)
+P3 = geoMod.addPoint(210, 0, 0)
+P4 = geoMod.addPoint(210, depth, 0)
 
 L1 = geoMod.addLine(P1, P3)
 L2 = geoMod.addLine(P3, P4)
@@ -51,8 +50,8 @@ LoopMouth = geoMod.addCurveLoop([L1, L2, L3, L4]) # found via GUI
 SurfMouth = geoMod.addPlaneSurface([LoopMouth])
 geoMod.synchronize()
 
-geoMod.mesh.setTransfiniteCurve(L1, 4)
-geoMod.mesh.setTransfiniteCurve(L3, 4)
+geoMod.mesh.setTransfiniteCurve(L1, 5)
+geoMod.mesh.setTransfiniteCurve(L3, 5)
 geoMod.mesh.setTransfiniteCurve(L2, 0)
 geoMod.mesh.setTransfiniteCurve(L4, 0)
 geoMod.mesh.setTransfiniteSurface(SurfMouth, "Left")
@@ -61,9 +60,9 @@ geoMod.synchronize()
 
 
 # MODIFY: grid grading on all other surfaces
-N = 12 					# number elements
-r = 1.09 				# ratio
-d = [10] 				# first element thickness
+N = 13 					# number elements
+r = 1.124 				# ratio
+d = [7] 				# first element thickness
 for i in range(1, N): 
 	d.append(d[-1] + (d[0]) * r**i)
 d.pop()
@@ -72,8 +71,8 @@ h_norm = []
 for d_i in d:
 	h_norm.append(d_i / 200)
 
-extrude1 = geoMod.extrude([(1, L4)], -190, 0, 00, [19], recombine = True)
-extrude2 = geoMod.extrude([(1, L2)], 190, 0, 00, [19], recombine = True)
+extrude1 = geoMod.extrude([(1, L4)], -190, 0, 00, [27], recombine = True)
+extrude2 = geoMod.extrude([(1, L2)], 190, 0, 00, [27], recombine = True)
 extrude3 = geoMod.extrude([(1, 5)], 
 						  -200, 0, 00, 
 						  numElements = [1] * N, 
@@ -88,9 +87,9 @@ geoMod.synchronize()
 
 
 # MODIFY: grid grading for streamwise direction
-N = 61 					# number elements
-r = 0.97291 			# ratio
-d = [50] 				# first element thickness
+N = 114 		# number elements
+r = 0.992 		# ratio
+d = [20] 		# first element thickness
 for i in range(1, N): 
 	d.append(d[-1] + (d[0]) * r**i)
 d.reverse()
@@ -106,9 +105,9 @@ h_norm = []
 for h_i in heights:
 	h_norm.append(h_i / 1500)
 
-N2 = 32
-r = 1.0272
-d2 = [10]
+N2 = 29
+r = 1.05
+d2 = [8]
 for i in range(1, N2):
 	d2.append(d2[-1] + (d2[0]) * r**i)
 d2.pop()
@@ -143,17 +142,18 @@ gmsh.model.addPhysicalGroup(3, entities)
 # group surfaces into regions where same flow conditions can be applied 
 # (ie. assign same boundary conditions to surfaces within this group). Surfaces
 # found via GUI
-gmsh.model.addPhysicalGroup(2, [152], name = "mouth")
+gmsh.model.addPhysicalGroup(2, [42], name = "mouth")
 gmsh.model.addPhysicalGroup(2, [209, 165, 139, 195, 239, 99, 55, 29, 85, 129,
 								121, 77, 37, 63, 107, 217, 173, 147, 187, 231], 
-								name = "empty")
-gmsh.model.addPhysicalGroup(2, [64, 42, 86, 130, 125, 102, 108, 213, 
-								218, 174, 196, 240, 235, 103], name = "atmos")
+							name = "empty")
+gmsh.model.addPhysicalGroup(2, [125, 102, 213, 152, 218, 174, 196, 240, 235, 103], 
+							name = "atmos")
+gmsh.model.addPhysicalGroup(2, [130, 86, 64, 108], name = "wall")
 
 
 # mesh, save, then view mesh (in GUI)
 gmsh.model.mesh.generate(3)
 gmsh.option.setNumber("Mesh.MshFileVersion",2.0) 
-#gmsh.write(nameModel + ".msh")
+gmsh.write(nameModel + ".msh")
 gmsh.fltk.run()
 gmsh.finalize()
